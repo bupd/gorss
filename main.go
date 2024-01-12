911364c3d60dce4f06a91c8b9d4d7622d2dde347
bupd <bupdprasanth@gmail.com>
2024-01-12 12:38:13 +0530

    mod: Added necessary functions.
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- main.go
+++ main.go
@@ -2,15 +2,62 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"time"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
-func greet(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5XX error:", msg)
+	}
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJson(w, code, errResponse{
+		Error: msg,
+	})
 }
 
 func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("Port is not found in the .env")
+	}
+
+	fmt.Println("Port:", portString)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handleErr)
+
+	router.Mount("/v1", v1Router)
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
